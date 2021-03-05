f833b4a803aa06c1a74e7eb253c09d8a0606d172
Brandon81 <brandon.gutierrez@iut-rodez.fr>
2021-03-05 23:12:46 +0100

    Base scoreboard

--- src/main/java/LT5/Prison.java
+++ src/main/java/LT5/Prison.java
@@ -7,7 +7,7 @@ public final class Prison extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Chargement du Scoreboard");
     }
 
     @Override
